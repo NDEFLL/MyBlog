881c6fc7e859ad9856d65dff258a4bf4bd2257b9
NDEFLL <ndefl8888@gmail.com>
2025-04-06 16:52:28 +0800

    settings文件修改

--- MyBlog/settings.py
+++ MyBlog/settings.py
@@ -69,6 +69,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.static',  # 收集静态文件确保有这一行
             ],
         },
     },
