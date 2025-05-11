0b6a941c2bc75c79409acbdc82685558e017a068
NDEFLL <ndefl8888@gmail.com>
2025-05-11 16:16:35 +0800

    更新

--- blog/forms.py
+++ blog/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.core.validators import FileExtensionValidator
 
-from .models import Article
+from .models import Article, Message
 from django_ckeditor_5.widgets import CKEditor5Widget
 
 class ArticleForm(forms.ModelForm):
@@ -15,5 +15,16 @@ class ArticleForm(forms.ModelForm):
                 config_name='default'
             ),
             'is_published': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+        }
+
+class MessageForm(forms.ModelForm):
+    class Meta:
+        model = Message
+        fields = ['name', 'contact_type', 'contact_info', 'content']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': '请输入您的姓名'}),
+            'contact_type': forms.Select(attrs={'class': 'form-control', 'id': 'contact-type'}),
+            'contact_info': forms.TextInput(attrs={'class': 'form-control', 'placeholder': '请输入您的联系方式'}),
+            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 5, 'placeholder': '请输入您的留言内容'}),
         }
 
