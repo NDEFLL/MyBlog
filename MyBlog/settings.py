a3449a36c11f9c6991201e99c54e8d421dcc6fe2
NDEFLL <ndefl8888@gmail.com>
2025-05-12 14:26:55 +0800

    Update settings.py

--- MyBlog/settings.py
+++ MyBlog/settings.py
@@ -16,6 +16,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+DATA_UPLOAD_MAX_MEMORY_SIZE = 1024 * 1024 * 50  # 50MB
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
